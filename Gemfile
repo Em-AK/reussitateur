47f6f0990b8ba581ecb274b9177bec71626c6f24
em-ak <emilien.ak@gmail.com>
2013-12-14 15:36:49 +0100

    Authenticate Sailors with devise
      * Define Sailor object
      * Authentification with the devise gem

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
+gem 'devise'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
---
+++ app/models/sailor.rb
@@ -0,0 +1,6 @@
+class Sailor < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
