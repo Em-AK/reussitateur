3d83df8836108c0ccd94bcb9ca515847cdad536a
em-ak <emilien.ak@gmail.com>
2013-12-14 19:24:27 +0100

    CRUD actions for Crew

--- app/controllers/crews_controller.rb
+++ app/controllers/crews_controller.rb
@@ -1,22 +1,49 @@
 class CrewsController < ApplicationController
+  before_action :set_crew, only: [:show, :edit, :update, :destroy]
+
   def index
+    @crews = Crew.all
   end
 
   def show
   end
 
   def new
+    @crew = Crew.new
   end
 
   def create
+    @crew = Crew.new(crew_params)
+    if @crew.save
+      redirect_to @crew, notice: "L'équipage a bien été créé."
+    else
+      render action: 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @crew.update(crew_params)
+      redirect_to @crew, notice: "L'équipage à bien été mis à jour."
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    @crew.destroy
+    redirect_to crews_url
   end
+
+  private
+
+    def set_crew
+      @crew = Crew.find(params[:id])
+    end
+
+    def crew_params
+      params.require(:crew).permit(:title, :description)
+    end
 end
